c7caf332f0a5bbe5431c2c655a5e81819cd09e84
willy <willypratama231@gmail.com>
2023-10-04 21:28:16 +0700

    create custom hooks, useColorMode and useLocalStorage

---
+++ src/hooks/useColorMode.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+// import local storage hooks
+import useLocalStorage from "./useLocalStorage";
+
+export default function useColorMode() {
+  const [colorMode, setColorMode] = useLocalStorage("color-mode", "light");
+
+  useEffect(() => {
+    const className = "dark";
+    const setDark = document.documentElement.classList;
+
+    colorMode === "dark" ? setDark.add(className) : setDark.remove(className);
+  }, [colorMode]);
+
+  return [colorMode, setColorMode];
+}
