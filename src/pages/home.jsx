358d0fea323f9fb17e7a1bce311a1ca8a0aed2ff
willy <willypratama231@gmail.com>
2023-10-07 15:25:14 +0700

    Change position for components in home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Fragments/Navbar";
 import Hero from "../components/Layouts/Hero";
 import AboutUs from "../components/Layouts/AboutUs";
 import Footer from "../components/Layouts/Footer";
-import Promo from "../components/Layouts/promo";
+import Promo from "../components/Layouts/Promo";
 import Activities from "../components/Layouts/Activities";
 import ContactUs from "../components/Layouts/ContactUs";
 
