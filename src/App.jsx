94b0e30af1beb8f4a1b941e8f9ac5c2634b4a86c
willy <willypratama231@gmail.com>
2023-10-07 15:24:16 +0700

    Update dependencies, tailwind config, and add new page in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import { useRoutes } from "react-router-dom";
 import HomePage from "./pages/home";
+import CategoryPage from "./pages/category";
+import LoginPage from "./pages/login";
 
-const routes = [{ path: "/", element: <HomePage /> }];
+const routes = [
+  { path: "/", element: <HomePage /> },
+  { path: "/category", element: <CategoryPage /> },
+  { path: "/login", element: <LoginPage /> },
+];
 
 function App() {
   const element = useRoutes(routes);
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,20 @@ export default {
         xl: "1024px",
         "2xl": "1200px",
       },
+      colors: {
+        primaryColor: "#015AB8",
+      },
       transitionDuration: {
         400: "0.4s",
+      },
+      animation: {
+        slide: "slide 25s linear infinite",
+      },
+      keyframes: {
+        slide: {
+          "0%,100%": { transform: "translateX(5%)" },
+          "50%": { transform: "translateX(-120%)" },
+        },
       },
     },
   },
