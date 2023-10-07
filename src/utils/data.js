db764205b97c9f18afa198247a5af78354a43244
willy <willypratama231@gmail.com>
2023-10-07 15:22:33 +0700

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -7,6 +7,9 @@ export const navItems = [
   { link: "FAQ", path: "faq" },
 ];
 
+export const LoginImage =
+  "https://images.unsplash.com/photo-1600971569276-1618e55af2ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1907&q=80";
+
 export const HeroImage =
   "https://i.pinimg.com/originals/28/0d/61/280d6187a916255757daaf67dd3cdc2f.jpg";
 
